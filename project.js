00041fb7ea2acfcacc9b534bc7d954208d951187
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-09-10 12:52:34 +0530

    add mark symbols

--- project.js
+++ project.js
@@ -6,6 +6,7 @@ const project = [
       "A game built using CLI with JavaScript language for fun. The game asks you some basics questions about me.",
     liveLink: "https://replit.com/@pritamkr/How-well-you-know-me#index.js",
     gitLink: "https://github.com/pritam-kr/you-know-me",
+    mark: '#markOne'
   },
   {
     id: "1",
@@ -14,6 +15,7 @@ const project = [
       "A quiz application on CLI. It asks some questions about MARVEL TV SERIES.",
     liveLink: "https://replit.com/@pritamkr/MarvelQuizs#index.js",
     gitLink: "https://github.com/pritam-kr/MarvelQuizs",
+    mark: '#markTwo'
   },
   {
     id: "1",
@@ -22,6 +24,7 @@ const project = [
       "Built a web portfolio using only HTML and CSS and hosted on netlify.",
     liveLink: "https://neogcamp-portfolio-of-pritam.netlify.app/",
     gitLink: "https://github.com/pritam-kr/neogcamp-portfoli",
+    mark: '#markFour + #markFive'
   },
   {
     id: "1",
@@ -29,6 +32,7 @@ const project = [
     projectDes: "A translation app using HTML, CSS and VanilaJS.",
     liveLink: "https://banana-talk-by-pritam.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Banana-talk",
+    mark: '#markSix'
   },
   {
     id: "1",
@@ -37,6 +41,7 @@ const project = [
       " An application that can translate your English language into a pirate language.",
     liveLink: "https://pirate-translator-by-pritam.netlify.app/",
     gitLink: "https://github.com/pritam-kr/pirate-speak-translator",
+    mark: '#markSeven'
   },
   {
     id: "1",
@@ -45,6 +50,7 @@ const project = [
       "This app is my first react app using code sandbox. So. This app will help you to understand your emoji emotions.",
     liveLink: "https://csb-t1k4t.netlify.app/",
     gitLink: "https://github.com/pritam-kr/emoji-emotions",
+    mark: '#markEight'
   },
   {
     id: "1",
@@ -53,6 +59,7 @@ const project = [
       "An application that can show you the best books on specific topics, also it'll show you the rating of these books. It is built on code sandbox.",
     liveLink: "https://csb-pfvhx.netlify.app/",
     gitLink: "https://github.com/pritam-kr/my-favorite-book-with-react",
+    mark: '#markNine'
   },
   {
     id: "1",
@@ -61,6 +68,7 @@ const project = [
       "This is a simple movie list app when you click on specific tags such as Comedy, Horror, Action. It will show to all movies related to these tags.",
     liveLink: "https://csb-ein4f.netlify.app/",
     gitLink: "https://github.com/pritam-kr/my-movies",
+    mark: '#markNine task'
   },
   {
     id: "1",
@@ -69,6 +77,7 @@ const project = [
       "Built an app using VanilajS to check your birthdate is Palindrome or not, If it is not a palindrome, then it will show the nearest palindrome date and by how many days it got missed.",
     liveLink: "https://flamboyant-wilson-ca77c3.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Is_your_birthday_is_palindrome",
+    mark: '#markThirteen'
   },
   {
     id: "1",
@@ -77,6 +86,7 @@ const project = [
       "A triangle web app using HTML, CSS, and VanilaJS. That can find the area of triangle and quizzes on the triangle.",
     liveLink: "https://triangle-app-by-pritam.netlify.app/index.html",
     gitLink: "https://github.com/pritam-kr/Triangle",
+    mark: '#markTwelve'
   },
   {
     id: "1",
@@ -84,6 +94,7 @@ const project = [
     projectDes: "A vanilaJs application to know how lucky your birth date is?",
     liveLink: "https://wizardly-nobel-ae8892.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Lucky_birthday",
+    mark: '#markEleven'
   },
   {
     id: "1",
@@ -92,6 +103,7 @@ const project = [
       "This app takes input as bill amount and cash given by customer, and shows output of number of notes to return.",
     liveLink: "https://elastic-morse-c51461.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Cash-manager",
+    mark: '#markTen'
   },
   {
     id: "1",
@@ -100,6 +112,7 @@ const project = [
       "An application that can tell you your loss and profit on the share market.",
     liveLink: "https://dreamy-swanson-2de3d4.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Stock",
+    mark: '#markFourteen'
   },
   {
     id: "1",
@@ -107,6 +120,7 @@ const project = [
     projectDes: "Check your string is palindrome or not?",
     liveLink: "https://infallible-saha-f826c6.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Check_palindrome_string",
+    mark: '#practice'
   },
   {
     id: "1",
@@ -114,6 +128,7 @@ const project = [
     projectDes: "Covid tracker web app usig javaScript and API",
     liveLink: "https://pritam-kr.github.io/Covid_tracker/",
     gitLink: "https://github.com/pritam-kr/Covid_tracker",
+    mark: '#project'
   },
 
   {
@@ -122,6 +137,7 @@ const project = [
     projectDes: "A web app which show your github account.",
     liveLink: "https://pritam-kr.github.io/Github-profile/",
     gitLink: "https://github.com/pritam-kr/Github-profile",
+    mark: '#project'
   },
   {
     id: "1",
@@ -129,6 +145,7 @@ const project = [
     projectDes: "A web app which show your github profile.",
     liveLink: "https://pritam-kr.github.io/github-profile-card/",
     gitLink: "https://github.com/pritam-kr/github-profile-card",
+    mark: '#project'
   },
   {
     id: "1",
@@ -136,6 +153,7 @@ const project = [
     projectDes: "know leap year by just putting year",
     liveLink: "https://admiring-nightingale-b138b3.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Leap_year",
+    mark: '#bonusTask'
   },
   {
     id: "1",
@@ -143,6 +161,7 @@ const project = [
     projectDes: "Is your birth day a prime number?",
     liveLink: "https://brave-fermat-03180c.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Prime_Number",
+    mark: '#bonusTask'
   },
   {
     id: "1",
@@ -150,6 +169,7 @@ const project = [
     projectDes: "Check stock's profit or loss with real time current price using API.",
     liveLink: "https://angry-thompson-a501dc.netlify.app/",
     gitLink: "https://github.com/pritam-kr/Stock_profit_loass_indicator",
+    mark: '#bonusTask'
   }
 ];
 
@@ -158,6 +178,7 @@ project.forEach((el) => {
   box += `
       <div class="project-box">
         <h1 class="project-name">${el.projectName}</h1>
+        <p style="font-size: 0.8rem; font-weight: bold; color: rgba(0, 0, 0, 0.763);">${el.mark}</p>
         <p class="project-des">${el.projectDes}</p>
         <button class="btn btn-live"><a href="${el.liveLink}">Live</a></button><button class="btn btn-git"><a href="${el.gitLink}">Github</a></button>
       </div>
